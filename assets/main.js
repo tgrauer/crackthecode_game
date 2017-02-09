390835e49223b36a4c3ec3d649e382dfee8440da
Thomas Grauer <tgrauer1@me.com>
2017-02-08 19:26:17 -0500

    level difficulty changes, display points earned

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,46 @@
 let answer = document.getElementById('answer').value;
 let attempt = document.getElementById('attempt').value;
 let results = document.getElementById('results');
-let max_attempts=10;
 let attempts_left=10;
 let code = document.getElementById('code').getElementsByTagName('strong')[0];
+let revealCode;
 let input = document.getElementById('user-guess');
+let progress='';
 let points=0;
 let level = 1;
 let pointsHolder=document.getElementById('points').getElementsByTagName('span')[0];
 let levelHolder=document.getElementById('points').getElementsByTagName('span')[1];
+let codelength=3;
+let multiplier=100;
 
 if(answer=='' || attempt==''){
 	 setHiddenFields();
 }
 
 function setHiddenFields(){
-	answer=Math.floor(Math.random(0,1)*10000);
+	multiplier='1';
+	for(var i=0;i<codelength;i++){
+		multiplier+='0';
+	}
+
+	multiplier= parseFloat(multiplier);
+
+	answer=Math.floor(Math.random(0,1)*multiplier);
 	answer=answer.toString();
 	attempt=0;
-	while(answer.length<4){
+	attempts_left=10;
+	while(answer.length<codelength){
 		answer='0'+answer;
 	}
 	document.getElementById('answer').value=answer;
 	document.getElementById('user-guess').focus();
-	attempts_left=max_attempts;
+	console.log(answer);
 }
 
 function guess() {
-   
+
+	// code.innerHTML="";
+
 	if(!validateInput(input.value)){
 		return false;
 	}else{
@@ -37,13 +50,14 @@ function guess() {
 	if(getResults(input.value)){
 		document.getElementById('message').innerHTML='YOU WIN !!!!';
 		document.getElementById('message').className='message alert alert-success';
-		showAnswer(true);
-	}else if(attempt>=max_attempts){
+		levelOver(true);
+	}else if(attempt>=10){
 		document.getElementById('message').innerHTML='GAME OVER !!!!';
 		document.getElementById('message').className='message alert alert-danger';
-		showAnswer(getResults(input.value));
+		levelOver(getResults(input.value));
 	}else{
 		attempts_left--;
+		document.getElementById('message').style.display='block';
 		document.getElementById('message').innerHTML=attempts_left+ " Attempts left";
 		if(!document.getElementById('message').classList.contains('alert')){
 			document.getElementById('message').className +=' alert';
@@ -57,20 +71,22 @@ function guess() {
 }
 
 function validateInput(guess){
-	if(guess.length==4){		
+		
+	if(guess.length==codelength){	
+
 		document.getElementById('message').innerHTML='';
 		return true;
 	}else{
+		
 		document.getElementById('user-guess').focus();
 		document.getElementById('message').className +=' alert';
 		document.getElementById('message').className +=' alert-danger';
-		document.getElementById('message').innerHTML="Guess must be exactly 4 characters long.";
+		document.getElementById('message').innerHTML="This code for this level is "+ codelength +" characters long.";
 		return false;
 	}
 }
 
 function getResults(input){
-
 	var correct=0;
 	var html = '<div class="row guessrow"><div class="col-xs-6"><b>' + input + '</b></div><div class="col-xs-6">';
 
@@ -87,7 +103,7 @@ function getResults(input){
 
 	html += '</div></div>';
 	results.innerHTML += html;
-
+	
 	if(correct==input.length){
 		return true;
 	}else{
@@ -95,13 +111,15 @@ function getResults(input){
 	}
 }
 
-function showAnswer(gameover){
-	points=attempts_left*100 +points;
+function levelOver(gameover){
+	var points_earned=attempts_left*100 +points*level;
+	points=points+points_earned;
 	pointsHolder.innerHTML=points;
 
 	if(gameover){
 		code.className+=' success';
 		document.getElementById('nextlevel-div').style.display='block';
+		document.getElementsByClassName('points_earned')[0].innerHTML=points_earned +' POINTS';
 	}else{
 		code.className+=' failure';
 		document.getElementById('replay-div').style.display='block';
@@ -112,18 +130,22 @@ function showAnswer(gameover){
 }
 
 function nextlevel(){
-	code.innerHTML="????";
+	
 	code.classList.remove("success");
 
 	document.getElementById('message').innerHTML="";
 	document.getElementById('message').classList.remove("alert");
 	document.getElementById('message').classList.remove("alert-success");
-	document.getElementById('message').style.display='none';
 	document.getElementById('nextlevel-div').style.display='none';
 	document.getElementById('guessing-div').style.display='block';
 	level++;
+	codelength++;
 	levelHolder.innerHTML=' '+level;
-	max_attempts--;
+	code.innerHTML="";
+
+	for(var i=0;i<codelength;i++){
+		code.innerHTML+='?';
+	}
 
 	var guessedRows = document.getElementsByClassName('guessrow');
 	while (guessedRows[0]) {
